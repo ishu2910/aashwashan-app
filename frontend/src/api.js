c98fb0bf1ff7640bbd82c0323b7097407b2e5135
ishu2910 <Ishangoel001@gmail.com>
2026-05-11 10:31:02 +0530

    Fix API base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://aashwashan-app-1.onrender.com",
+  baseURL: "https://aashwashan-app-1.onrender.com/api",
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
